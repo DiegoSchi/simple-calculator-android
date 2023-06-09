c41e4799b4d93c43d2aa44efeead856ace8c81ff
Diego Schiaffini <lubikogarcia@gmail.com>
2023-06-09 00:17:12 -0600

    feature/create-ui/add-ui-elements

--- app/src/main/java/com/example/calculadora/MainActivity.kt
+++ app/src/main/java/com/example/calculadora/MainActivity.kt
@@ -2,10 +2,37 @@ package com.example.calculadora
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val tituloActivity : TextView = findViewById(R.id.textViewTitulo)
+        val numerosCaja : EditText = findViewById(R.id.editTextNumeros)
+        val btnSumar : Button = findViewById(R.id.botonSuma)
+        val btnRestar : Button = findViewById(R.id.botonResta)
+        val btnMultiplicar : Button = findViewById(R.id.botonMultiplicacion)
     }
+
+
+/*
+    fun sumar() : Double {
+
+    }
+
+    fun restar() : Double {
+
+    }
+
+    fun multiplicar() : Double  {
+
+    }
+
+
+ */
 }
