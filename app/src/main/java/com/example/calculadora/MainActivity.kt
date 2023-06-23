67d48048120fa3eefedd34bf19ca2947b98c44d2
Diego Schiaffini <lubikogarcia@gmail.com>
2023-06-23 00:14:47 -0600

    new features

--- app/src/main/java/com/example/calculadora/MainActivity.kt
+++ app/src/main/java/com/example/calculadora/MainActivity.kt
@@ -2,29 +2,50 @@ package com.example.calculadora
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
+import com.example.calculadora.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding : ActivityMainBinding
+    private var listaDeNumeros = mutableListOf<Double>()
+    private var acumulador : Double = 0.0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val tituloActivity : TextView = findViewById(R.id.textViewTitulo)
-        val numerosCaja : EditText = findViewById(R.id.editTextNumeros)
-        val btnSumar : Button = findViewById(R.id.botonSuma)
-        val btnRestar : Button = findViewById(R.id.botonResta)
-        val btnMultiplicar : Button = findViewById(R.id.botonMultiplicacion)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.botonSuma.setOnClickListener {
+
+            listaDeNumeros.add(binding.editTextNumeros.text.toString().toDouble())
+            binding.editTextNumeros.setText("")
+
+        }
+
+        binding.botonResta.setOnClickListener {
+
+            sumar()
+        }
+
+        binding.botonMultiplicacion.setOnClickListener {
+
+        }
     }
 
 
+   fun sumar(){
+
+       for (numero in listaDeNumeros) {
+            acumulador+= numero
+       }
+
+       binding.editTextNumeros.setText(acumulador.toString())
+       listaDeNumeros.clear()
+
+    }
 /*
-    fun sumar() : Double {
-
-    }
-
     fun restar() : Double {
 
     }
