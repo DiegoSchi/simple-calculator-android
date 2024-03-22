1c41f3a92fad25c1305cb32e3a6f2baec0c0ad86
Diego Schiaffini <contacto.dmschiaffini@gmail.com>
2024-03-21 18:12:39 -0600

    Logic and UI

--- app/src/main/java/com/example/calculadora/MainActivity.kt
+++ app/src/main/java/com/example/calculadora/MainActivity.kt
@@ -3,20 +3,21 @@ package com.example.calculadora
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.example.calculadora.data.Operations
 import com.example.calculadora.databinding.ActivityMainBinding
+import kotlin.math.abs
 
 class MainActivity : AppCompatActivity() {
 
     //Binding
-    private lateinit var binding : ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
+
     //Variables globales
-    private var listaDeNumeros = mutableListOf<Double>()
-    private var acumulador : Double = 0.0
-    private var id : String = "DEFAULT"
+    private var acumulador: Double = 0.0
+    private var id: Operations = Operations.DEFAULT
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
@@ -24,9 +25,10 @@ class MainActivity : AppCompatActivity() {
         //Boton Suma
         binding.botonSuma.setOnClickListener {
 
-            id = "SUMA"
-            if(!binding.editTextNumeros.text.toString().equals("")) {
-                listaDeNumeros.add(binding.editTextNumeros.text.toString().toDouble())
+            id = Operations.SUMA
+            if (binding.editTextNumeros.text.toString() != "") {
+                val numeroActual = binding.editTextNumeros.text.toString().toDouble()
+                sumar(numeroActual)
                 binding.editTextNumeros.setText("")
             } else {
                 val toast = Toast.makeText(this, "INTRODUCE UN NUMERO", Toast.LENGTH_SHORT)
@@ -36,21 +38,22 @@ class MainActivity : AppCompatActivity() {
         }
         //Boton Resta
         binding.botonResta.setOnClickListener {
-            id = "RESTA"
-            if(!binding.editTextNumeros.text.toString().equals("")) {
-                listaDeNumeros.add(binding.editTextNumeros.text.toString().toDouble())
+            id = Operations.RESTA
+            if (binding.editTextNumeros.text.toString() != "") {
+                val numeroActual = binding.editTextNumeros.text.toString().toDouble()
+                restar(numeroActual)
                 binding.editTextNumeros.setText("")
             } else {
-                val toast = Toast.makeText(this, "INTRODUCE UN NUMERO", Toast.LENGTH_SHORT)
-                toast.show()
+                Toast.makeText(applicationContext, "INTRODUCE UN NÚMERO", Toast.LENGTH_SHORT).show()
             }
 
         }
         //Boton Multiplicación
         binding.botonMultiplicacion.setOnClickListener {
-            id = "MULTIPLICACION"
-            if(!binding.editTextNumeros.text.toString().equals("")) {
-                listaDeNumeros.add(binding.editTextNumeros.text.toString().toDouble())
+            id = Operations.MULTIPLICACION
+            if (binding.editTextNumeros.text.toString() != "") {
+                val numActual = binding.editTextNumeros.text.toString().toDouble()
+                multiplicar(numActual)
                 binding.editTextNumeros.setText("")
             } else {
                 val toast = Toast.makeText(this, "INTRODUCE UN NUMERO", Toast.LENGTH_SHORT)
@@ -59,54 +62,88 @@ class MainActivity : AppCompatActivity() {
         }
         //Boton Limpiar
         binding.btnDelete.setOnClickListener {
-
-            binding.editTextNumeros.setText("")
-            listaDeNumeros.clear()
+            id = Operations.DEFAULT
             acumulador = 0.0
+            binding.editTextNumeros.setText("")
         }
         //Boton Resultado
         binding.btnResultado.setOnClickListener {
 
-            var res = 0.0
+            val res: Double
 
-            when(id) {
-                "SUMA" -> res = sumar()
-                "RESTA" -> res = restar()
-                "MULTIPLICACION" -> res = multiplicar()
+            when (id) {
+                Operations.SUMA -> {
+                    if (binding.editTextNumeros.text.toString() != "") {
+                        res = sumar(binding.editTextNumeros.text.toString().toDouble())
+                        binding.editTextNumeros.setText(res.toString())
+                        acumulador = 0.0
+                    } else {
+                        res = sumar(ZERO)
+                        binding.editTextNumeros.setText(res.toString())
+                        acumulador = 0.0
+                    }
+                }
+
+                Operations.RESTA -> {
+                    if (binding.editTextNumeros.text.toString() != "") {
+                        res = restar(binding.editTextNumeros.text.toString().toDouble())
+                        binding.editTextNumeros.setText(res.toString())
+                        acumulador = 0.0
+                    } else {
+                        res = restar(ZERO)
+                        binding.editTextNumeros.setText(res.toString())
+                        acumulador = 0.0
+                    }
+                }
+
+                Operations.MULTIPLICACION -> {
+                    if (binding.editTextNumeros.text.toString() != "") {
+                        res = multiplicar(binding.editTextNumeros.text.toString().toDouble())
+                        binding.editTextNumeros.setText(res.toString())
+                        acumulador = 0.0
+                    } else {
+                        res = multiplicar(ZERO)
+                        binding.editTextNumeros.setText(res.toString())
+                        acumulador = 0.0
+                    }
+                }
+
+                else -> Toast.makeText(this, "ERROR", Toast.LENGTH_SHORT).show()
             }
-
-            binding.editTextNumeros.setText(res.toString())
-            listaDeNumeros.clear()
-            acumulador = 0.0
-
         }
     }
 
 
-   private fun sumar() : Double{
+    private fun sumar(numActual: Double): Double {
 
-       for (numero in listaDeNumeros)  {
-            acumulador+= numero
-       }
+        acumulador += numActual
 
-       return acumulador
-    }
-
-    private fun restar() : Double {
-
-        for(numero in listaDeNumeros) {
-            acumulador-= numero
-        }
         return acumulador
     }
 
-    private fun multiplicar() : Double  {
+    private fun restar(numActual: Double): Double {
 
-        for(numero in listaDeNumeros) {
-            acumulador*= numero
+        if (acumulador == 0.0) {
+            acumulador = abs(acumulador - numActual)
+        } else {
+            acumulador -= numActual
         }
 
         return acumulador
+    }
 
+    private fun multiplicar(numActual: Double): Double {
+        if (acumulador == 0.0) {
+            acumulador = numActual * ONE
+        } else {
+            acumulador *= numActual
+        }
+
+        return acumulador
+    }
+
+    companion object {
+        private const val ZERO: Double = 0.0
+        private const val ONE: Double = 1.0
     }
 }
